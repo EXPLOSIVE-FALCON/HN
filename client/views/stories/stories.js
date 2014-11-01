023f3fe561884a84789fe7cd54bdd9af1819cea5
Kia Winslow <ksukop@gmail.com>
2014-10-31 17:25:39 -0700

    client: doing chart

--- client/views/stories/stories.js
+++ client/views/stories/stories.js
@@ -1,6 +1,4 @@
 angular.module('stories', ['storedData'])
   .controller('StoriesController', function ($scope, CurrentData) {
-    console.log('inside StoriesController');
     $scope.stories = CurrentData.stories;
-    console.log($scope.stories[0].title);
   });
